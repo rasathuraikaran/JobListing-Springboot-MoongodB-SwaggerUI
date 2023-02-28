a07acbebdd69eadbfa8e0e3d3612130308403738
Rasathurai Karan <73582838+rasathuraikaran@users.noreply.github.com>
2023-02-28 16:59:40 +0530

    initial commit

---
+++ src/main/java/com/example/joblistingspringbootmoongodb/JobListingSpringbootMoongodBApplication.java
@@ -0,0 +1,13 @@
+package com.example.joblistingspringbootmoongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobListingSpringbootMoongodBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobListingSpringbootMoongodBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/joblistingspringbootmoongodb/JobListingSpringbootMoongodBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joblistingspringbootmoongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobListingSpringbootMoongodBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
